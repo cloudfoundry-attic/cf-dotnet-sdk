79565665d0c0cf1373d977453fdd6786c8a0fcd1
Vlad Iovanov <vlad.iovanov@hp.com>
2015-02-17 22:49:09 -0800

    Initial commit.

---
+++ src/CloudFoundry.CloudController.V2.Client/Interfaces/IResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudFoundry.CloudController.V2.Interfaces
+{
+    public interface IResponse
+    {
+        Metadata EntityMetadata { get; set; }
+    }
+}
