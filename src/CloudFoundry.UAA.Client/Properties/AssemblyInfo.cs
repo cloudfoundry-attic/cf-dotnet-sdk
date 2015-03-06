8fc22c124f0157a4c95e45dc007a4477ef78ffa2
Mihai Buzgau <mihai.buzgau@hp.com>
2015-03-06 15:23:59 +0200

    merge with logyard commit

--- src/CloudFoundry.UAA.Client/Properties/AssemblyInfo.cs
+++ src/CloudFoundry.UAA.Client/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CloudFoundry.UAA.Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cloud Foundry .NET SDK UAA Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CloudFoundry.UAA.Client")]
