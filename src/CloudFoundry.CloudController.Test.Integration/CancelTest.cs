390e1fe8f0752db8f80762affc40b644ce7f337c
Mihai Buzgau <mihai.buzgau@hp.com>
2015-03-23 12:08:59 +0200

    add fake utility class for unit tests and cancellation token tests for CloudFoundryClient

---
+++ src/CloudFoundry.CloudController.Test.Integration/CancelTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
+using CloudFoundry.CloudController.V2.Client.Data;
+using CloudFoundry.Logyard.Client;
+
+namespace CloudFoundry.CloudController.Test.Integration
+{
+    [TestClass]
+    public class CancelTest
+    {
+        [TestMethod]
+        public void CancellationToken_test()
+        {
+            var cancelToken = new CancellationTokenSource();
+
+            var client = TestUtil.GetClient(cancelToken.Token);
+
+            try
+            {
+                var task = client.Info.GetInfo();
+                cancelToken.Cancel();
+                task.Wait();
+                Assert.Fail("Expected OperationCanceledException but no exception was thrown");
+
+            }
+            catch (AggregateException aex)
+            {
+                foreach (var ex in aex.Flatten().InnerExceptions)
+                {
+                    if (!(ex is OperationCanceledException))
+                    {
+                        Assert.Fail(string.Format("Expected OperationCanceledException but got {0}", ex.ToString()));
+                    }
+                }
+            }
+
+        }
+
+        [TestMethod]
+        public void CancellationTokenMultipleRequests_test()
+        {
+            var cancelToken = new CancellationTokenSource();
+
+            var client = TestUtil.GetClient(cancelToken.Token);
+            Task[] tasks = new Task[4];
+            try
+            {
+                for (int i = 0; i < tasks.Length; i++)
+                {
+                    tasks[i] = client.Info.GetInfo();
+                }
+
+                cancelToken.Cancel();
+                Task.WaitAll(tasks);
+                Assert.Fail("Expected OperationCanceledException but no exception was thrown");
+
+            }
+            catch (AggregateException aex)
+            {
+                foreach (var ex in aex.Flatten().InnerExceptions)
+                {
+                    if (!(ex is OperationCanceledException))
+                    {
+                        Assert.Fail(string.Format("Expected OperationCanceledException but got {0}", ex.ToString()));
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CancellationTokenMultipleRequestsCancel_test()
+        {
+            var cancelToken = new CancellationTokenSource();
+
+            var client = TestUtil.GetClient(cancelToken.Token);
+
+            Task<GetInfoResponse>[] tasks = new Task<GetInfoResponse>[4];
+            GetInfoResponse response = null;
+
+            try
+            {
+                for (int i = 0; i < tasks.Length; i++)
+                {
+                    tasks[i] = client.Info.GetInfo();
+                    if (i == 0)
+                    {
+                        response = tasks[0].Result;
+                        cancelToken.Cancel();
+                    }
+                }
+                Assert.IsNotNull(response);
+                Task.WaitAll(tasks);
+                Assert.Fail("Expected OperationCanceledException but no exception was thrown");
+
+            }
+            catch (AggregateException aex)
+            {
+                foreach (var ex in aex.Flatten().InnerExceptions)
+                {
+                    if (!(ex is OperationCanceledException))
+                    {
+                        Assert.Fail(string.Format("Expected OperationCanceledException but got {0}", ex.ToString()));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/CloudFoundry.CloudController.V2.Client.Test/MockClient.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Thinktecture.IdentityModel.Client;
+
+namespace CloudFoundry.CloudController.V2.Client.Test
+{
+    internal class MockClients
+    {
+        CloudFoundryClient cfClient;
+
+        internal string JsonResponse = "";
+        internal HttpStatusCode ExpectedStatusCode = HttpStatusCode.OK;
+
+
+        Func<HttpResponseMessage> customMessageFunc = null;
+
+        internal Func<HttpResponseMessage> CustomMessageFunc
+        {
+            set { customMessageFunc = value; }
+        }
+
+        internal CloudFoundryClient CreateCloudFoundryClient()
+        {
+            return CreateCloudFoundryClient(new CancellationToken());
+        }
+
+        internal CloudFoundryClient CreateCloudFoundryClient(CancellationToken cancellationToken)
+        {
+            System.Net.Http.Fakes.ShimHttpClient.AllInstances.SendAsyncHttpRequestMessageHttpCompletionOptionCancellationToken = sendHttpAsync;
+            this.cfClient = new CloudFoundryClient(new Uri("http://127.0.0.1.co"), cancellationToken);
+
+            return cfClient;
+        }
+
+        private Task<HttpResponseMessage> sendHttpAsync(HttpClient client, HttpRequestMessage message, HttpCompletionOption completionOption, CancellationToken cancelationToken)
+        {
+            if (customMessageFunc == null)
+            {
+                customMessageFunc = buildHttpResponseMessage;
+            }
+            var task = Task.Run(customMessageFunc);
+            return task;
+        }
+
+        private HttpResponseMessage buildHttpResponseMessage()
+        {
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+            responseMessage.Content = new StringContent(this.JsonResponse);
+            responseMessage.StatusCode = ExpectedStatusCode;
+            return responseMessage;
+        }
+
+
+    }
+}
