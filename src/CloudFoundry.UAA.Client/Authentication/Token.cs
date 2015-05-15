ca18d1030e9f9f7ddb4fb29783fe9bec906f7b27
Vlad Iovanov <vlad.iovanov@hp.com>
2015-05-15 16:05:16 -0700

    Expose user information in UAA token class

--- src/CloudFoundry.UAA.Client/Authentication/Token.cs
+++ src/CloudFoundry.UAA.Client/Authentication/Token.cs
@@ -5,19 +5,63 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Newtonsoft.Json;
 
     /// <summary>
     /// Class that represents a token items from Oauth v2.
     /// </summary>
     public class Token
     {
+        private string accessToken = null;
+
+        /// <summary>
+        /// Gets the username of the user associated with the token.
+        /// </summary>
+        /// <value>
+        /// A username.
+        /// </value>
+        public string UserName { get; private set; }
+
+        /// <summary>
+        /// Gets the email of the user associatedwith the token.
+        /// </summary>
+        /// <value>
+        /// An e-mail address.
+        /// </value>
+        public string Email { get; private set; }
+
+        /// <summary>
+        /// Gets the unique ID of the user associated with the token.
+        /// </summary>
+        /// <value>
+        /// The user's ID.
+        /// </value>
+        public string UserGuid { get; private set; }
+
         /// <summary>
         /// Gets the access token.
         /// </summary>
         /// <value>
         /// The access token.
         /// </value>
-        public string AccessToken { get; internal set; }
+        public string AccessToken 
+        {
+            get
+            {
+                return this.accessToken;
+            }
+
+            internal set
+            {
+                this.accessToken = value;
+
+                dynamic tokenInfo = Token.GetTokenInfo(this.accessToken);
+
+                this.UserName = tokenInfo == null ? null : tokenInfo["user_name"];
+                this.Email = tokenInfo == null ? null : tokenInfo["email"];
+                this.UserGuid = tokenInfo == null ? null : tokenInfo["user_id"];
+            }
+        }
 
         /// <summary>
         /// Gets the refresh token.
@@ -54,6 +98,52 @@
                     return false;
                 }
             }
+        }
+
+        /// <summary>
+        /// Decodes a token received from UAA.
+        /// Implementation based on https://github.com/cloudfoundry/cli/blob/master/cf/configuration/core_config/access_token.go
+        /// </summary>
+        /// <returns>A JSON object that contains the decoded information.</returns>
+        private static dynamic GetTokenInfo(string accessToken)
+        {
+            if (string.IsNullOrWhiteSpace(accessToken))
+            {
+                return null;
+            }
+
+            string[] encodedParts = accessToken.Split('.');
+
+            if (encodedParts.Length < 3)
+            {
+                return null;
+            }
+
+            string encodedTokenJson = encodedParts[1];
+            byte[] decodedBytes = Convert.FromBase64String(RestorePadding(encodedTokenJson));
+            string decodedJson = Encoding.UTF8.GetString(decodedBytes, 0, decodedBytes.Length);
+
+            return JsonConvert.DeserializeObject(decodedJson);
+        }
+
+        /// <summary>
+        /// Normalizes padding for a Base64 encoded string
+        /// Implementation based on https://github.com/cloudfoundry/cli/blob/master/cf/configuration/core_config/access_token.go
+        /// </summary>
+        /// <returns>A Base64 encoded string.</returns>
+        private static string RestorePadding(string seg)
+        {
+            switch (seg.Length % 4)
+            {
+                case 2: 
+                    seg = seg + "=="; 
+                    break;
+                case 3: 
+                    seg = seg + "="; 
+                    break;
+            }
+
+            return seg;
         }
     }
 }
