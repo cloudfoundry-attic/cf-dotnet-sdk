6d11beae338809b4bc106f9faeea3b18a4e57333
Daniel Carabas <daniel.carabas@hp.com>
2015-08-03 16:06:59 +0300

    Fix RetrieveFile method

---
+++ src/CloudFoundry.CloudController.V2.Client/Extensions/Data/DC_RetrieveFileResponse.cs
@@ -0,0 +1,46 @@
+﻿namespace CloudFoundry.CloudController.V2.Client.Data
+{
+    using System;
+
+    /// <summary>
+    /// Entity used to retrieve file informations
+    /// </summary>
+    public class RetrieveFileResponse
+    {
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the file.
+        /// </summary>
+        /// <value>
+        /// The size of the file.
+        /// </value>
+        public string FileSize
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the content of the file.
+        /// </summary>
+        /// <value>
+        /// The content of the file.
+        /// </value>
+        public string FileContent
+        {
+            get;
+            set;
+        }
+    }
+}
