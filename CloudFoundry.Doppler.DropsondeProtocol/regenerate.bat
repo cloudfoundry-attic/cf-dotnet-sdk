941ec02e19a1051e6c0ade5ec322c81aea1f26e5
Stefan Schneider <stefan.schneider@hpe.com>
2016-11-02 12:38:20 +0200

    Add regenrate helper script for dropsonde protocol

---
+++ CloudFoundry.Doppler.DropsondeProtocol/regenerate.bat
@@ -0,0 +1,15 @@
+cd "%~dp0..\packages\Google.Protobuf.Tools.*\tools\windows_x86\"
+set protoc=%CD%\protoc.exe
+cd "%~dp0"
+
+"%protoc%" dropsonde-protocol\events\envelope.proto --proto_path=dropsonde-protocol\events --include_imports  -o %TEMP%\dropsonde_proto_tmp.bin
+
+cd "%~dp0..\packages\protogen-net.*\tools"
+set protogen=%CD%\protogen.exe
+cd "%~dp0"
+
+"%protogen%" "-i:%TEMP%\dropsonde_proto_tmp.bin" -o:Envelope.cs -ns:CloudFoundry.Doppler.Client
+
+@echo -----
+@echo NOTE:
+@echo Rename the "event" namespace to "CloudFoundry.Doppler.DropsondeProtocol" manually
